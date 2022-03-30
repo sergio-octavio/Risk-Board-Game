dc9b1650eec5b0ce6cc9c4eeb379a6c0aec7e76a
Sergio Octavio <sergiooctaviomancebo@gmail.com>
2022-03-30 12:47:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Risk Board Game 
+classic risk game development using java, Observer Pattern, Model View Controller and GUI using Java Swing. 
---
+++ src/model/CardTypes.java
@@ -0,0 +1,10 @@
+package model;
+
+/**
+ * Enumeration that has all the card types
+ * @author bhargav
+ * @version 1.0
+ */
+public enum CardTypes {
+	Infantry, Cavalry, Artillery
+}
